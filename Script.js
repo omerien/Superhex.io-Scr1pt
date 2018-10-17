f82c6ef40cbde787aeba25ff79be9c231a940686
TBM13 <32852493+Truebamateo@users.noreply.github.com>
2018-10-16 22:43:44 -0300

    Release 1.8.0.1

--- Script.js
+++ Script.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Superhex.io Scr1pt
+// @name         Superhex.io Scr1pt - Zoom Hack and more
 // @namespace    Superhex.io Scr1pt
-// @version      1.8.0
-// @license      GPL-3.0
+// @version      1.8.0.1
+// @license      MIT
 // @homepageURL  https://github.com/Truebamateo/Superhex.io-Scr1pt
 // @contributionURL https://www.paypal.me/tbm13
 // @icon         http://superhex.io/img/fav_icon_1.png
@@ -14,6 +14,30 @@
 // @grant        none
 
 // ==/UserScript==
+
+/*
+MIT License
+
+Copyright (c) 2018 TBM13
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 var Text1TBM = localStorage.getItem("Text1TBM"), AdsTBM = localStorage.getItem("AdsTBM"), Language = localStorage.getItem("LangTBM"), currSkin = localStorage.getItem("selectedSkin"), currQuality = localStorage.getItem("quality"), zoomHack = localStorage.getItem("zoomTBM"), miniMode = localStorage.getItem("miniModeTBM");
 var style = document.createElement("style");
